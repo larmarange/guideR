bcff46ca026ff6ccacf41e213280f4e9c2ae6451
Joseph Larmarange <joseph@larmarange.net>
2025-02-06 10:52:37 +0100

    separate file

---
+++ R/unrowwise.R
@@ -0,0 +1,17 @@
+#' Remove row-wise grouping
+#'
+#' Remove row-wise grouping created with [dplyr::rowwise()] while preserving
+#' any other grouping declared with [dplyr::group_by()].
+#' @param data A data frame, data frame extension (e.g. a tibble), or a
+#' lazy data frame.
+#' @examples
+#' titanic |> dplyr::rowwise()
+#' titanic |> dplyr::rowwise() |> unrowwise()
+#'
+#' titanic |> dplyr::group_by(Sex, Class) |> dplyr::rowwise()
+#' titanic |> dplyr::group_by(Sex, Class) |> dplyr::rowwise() |> unrowwise()
+#' @export
+unrowwise <- function(data) {
+  data |>
+    dplyr::group_by(dplyr::pick(dplyr::all_of(dplyr::group_vars(data))))
+}
