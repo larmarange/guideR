4e3939951f688fca117022e2bb052a3e64734c8e
Joseph Larmarange <joseph@larmarange.net>
2025-10-24 11:18:24 +0200

    oups test file was pushed

--- tests/testthat/test-view_dictionary.R
+++ tests/testthat/test-view_dictionary.R
@@ -1,105 +1,32 @@
 test_that("plot_proportions() does not produce an error", {
-  expect_no_error(
-    titanic |>
-      plot_proportions(
-        Survived == "Yes",
-        overall_label = "All",
-        labels_color = "white"
-      )
-  )
-  expect_no_error(
-    titanic |>
-      plot_proportions(
-        Survived == "Yes",
-        by = c(Class, Sex),
-        fill = "lightblue"
-      )
-  )
-  expect_no_error(
-    titanic |>
-      plot_proportions(
-        Survived == "Yes",
-        by = c(Class, Sex),
-        fill = "lightblue",
-        flip = TRUE,
-        pvalues_test = "chisq"
-      )
-  )
-  skip_on_cran()
-  expect_no_error(
-    titanic |>
-      plot_proportions(
-        Survived == "Yes",
-        by = c(Class, Sex),
-        geom = "point",
-        color = "red",
-        size = 3,
-        show_labels = FALSE
-      )
-  )
-  expect_no_error(
-    titanic |>
-      srvyr::as_survey() |>
-      plot_proportions(
-        Survived == "Yes",
-        by = c(Class, Sex),
-        fill = "darksalmon",
-        color = "black",
-        show_overall_line = TRUE
-      )
-  )
-  d <- titanic
-  d$Sex[1:50] <- NA
-  expect_no_error(
-    d |> plot_proportions(Survived == "Yes", by = Sex)
-  )
-  expect_no_error(
-    d |> plot_proportions(Survived == "Yes", by = Sex, drop_na_by = TRUE)
-  )
+  skip_if_not_installed("DT")
+  skip_if_not_installed("htmltools")
+  skip_if_not_installed("htmlwidgets")
+
 
   expect_no_error(
-    iris |>
-      plot_proportions(Species == "versicolor", by = dplyr::contains("leng"))
+    iris |> view_dictionary()
+  )
+  expect_no_error(
+    iris |> view_dictionary(details = "none")
+  )
+  expect_no_error(
+    iris |> view_detailed_dictionary()
   )
 
+  d <- mtcars |>
+    labelled::set_na_range(
+      hp = c(800, 900)
+    ) |>
+    labelled::set_na_values(
+      cyl = 9:12
+    )
   expect_no_error(
-    iris |>
-      plot_proportions(
-        dplyr::tibble(
-          "Long sepal" = Sepal.Length > 6,
-          "Short petal" = Petal.Width < 1
-        ),
-        by = Species
-      )
+    d |> view_detailed_dictionary()
   )
 
+  skip_if_not_installed("gtsummary")
   expect_no_error(
-    titanic |>
-      plot_proportions(
-        (Survived == "Yes") |>  stratified_by(Sex),
-        by = Class
-      )
-  )
-
-  expect_no_error(
-    titanic |>
-      plot_proportions(
-        dummy_proportions(Class),
-        by = Sex,
-        mapping = ggplot2::aes(fill = level)
-      )
-  )
-
-  expect_no_error(
-    titanic |>
-      plot_proportions(
-        dplyr::tibble(
-          Survived = Survived == "Yes",
-          Male = Sex == "Male"
-        ),
-        by = c(Class),
-        mapping = ggplot2::aes(fill = condition),
-        free_scale = TRUE
-      )
+    gtsummary::trial |> view_detailed_dictionary()
   )
 })
