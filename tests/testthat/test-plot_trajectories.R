585a7b2c3f833209138582a06e98724af6a13dd5
Joseph Larmarange <joseph@larmarange.net>
2025-12-03 22:56:14 +0100

    trying another approach

--- tests/testthat/test-plot_trajectories.R
+++ tests/testthat/test-plot_trajectories.R
@@ -1,16 +1,3 @@
-# adapted from https://github.com/r-lib/vdiffr/issues/141
-if (nzchar(Sys.getenv("CI")) || !rlang::is_installed("vdiffr")) {
-  #if we are running tests remotely
-  # we are opting out of using vdiffr
-  # assigning a dummy function
-
-  expect_doppelganger <- function(...) {
-    testthat::skip("`vdiffr` tests not run")
-  }
-} else {
-  expect_doppelganger <- vdiffr::expect_doppelganger
-}
-
 test_that("plot_trajectories() and plot_periods() works", {
   d <- dplyr::tibble(
     id = c(1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3),
