87d452c162e590f2ae37fc472f515872d4bbd009
Joseph Larmarange <joseph@larmarange.net>
2026-07-07 18:23:45 +0200

    new version of contributions()

--- tests/testthat/test-contributions.R
+++ tests/testthat/test-contributions.R
@@ -6,6 +6,9 @@ test_that("contributions() does not produce an error", {
     m |> contributions()
   )
   expect_no_error(
+    m |> contributions(type = "I")
+  )
+  expect_no_error(
     m |> tbl_contributions()
   )
   expect_no_error(
@@ -21,4 +24,18 @@ test_that("contributions() does not produce an error", {
     m2 |> tbl_contributions(show = "Relative", notes = FALSE)
   )
 
+  m3 <- survey::svyglm(
+    Survived == "Yes" ~ Class + Sex + Age,
+    design = srvyr::as_survey(titanic),
+    family = quasibinomial
+  )
+  expect_no_error(
+    m3 |> contributions()
+  )
+  expect_no_error(
+    m3 |> tbl_contributions(type = "drop1")
+  )
+  expect_no_error(
+    m3 |> tbl_contributions(type = "add1")
+  )
 })
