5261763d680d13c363200526b99960c8aeec5e62
Joseph Larmarange <joseph@larmarange.net>
2026-08-11 11:15:28 +0200

    adding a check on MAIHDA

--- R/bootstrapped_maihda.R
+++ R/bootstrapped_maihda.R
@@ -2,7 +2,7 @@
 #'
 #' @description A list as returned by [calculate_partially_adjusted_maihda()]
 #' @keywords datasets
-#' @examples
+#' @examplesIf rlang::is_installed("MAIHDA")
 #' # m <- MAIHDA::maihda(
 #' #   Survived ~ Age + Sex + Class + (1 | Age:Sex:Class),
 #' #   data = titanic,
@@ -16,8 +16,6 @@
 #' #     bootstrap_vpc = TRUE
 #' #   )
 #'
-#' \donttest{
 #' bootstrapped_maihda |>
 #'   tbl_maihda(exponentiate = TRUE)
-#' }
 "bootstrapped_maihda"
