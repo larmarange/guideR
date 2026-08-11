a2c4319c8dc0e76756311c76589da869c04f5a72
Joseph Larmarange <joseph@larmarange.net>
2026-08-11 10:01:13 +0200

    bootstrapped_maihda

---
+++ data-raw/DATASET.R
@@ -0,0 +1,11 @@
+m <- MAIHDA::maihda(
+  Survived ~ Age + Sex + Class + (1 | Age:Sex:Class),
+  data = titanic,
+  family = binomial
+)
+
+bootstrapped_maihda <-
+  m |>
+  calculate_partially_adjusted_maihda(bootstrap_pcv = TRUE, bootstrap_vpc = TRUE)
+
+usethis::use_data(bootstrapped_maihda, overwrite = TRUE)
